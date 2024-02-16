bf6272f96c331432fc1f23265005c455b70fcd7a
Arturs <arturs.vanags320@gmail.com>
2024-02-16 22:57:07 +0200

    Fix import statement in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import '@/styles/globals.css';
 
 import { fontHeading, fontMono, fontSans } from '@/lib/fonts';
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from '@/components/theme-provider';
 import { TailwindIndicator } from '@/lib/helpers/tailwind-indicator';
 import { cn } from '@/lib/utils';
 
 import { siteConfig } from '@/config/site';
 
 import { Metadata } from 'next';
-
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -43,8 +42,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-
-            <main className="relative">{children}</main>
+          <main className="relative">{children}</main>
           <TailwindIndicator />
         </ThemeProvider>
       </body>
