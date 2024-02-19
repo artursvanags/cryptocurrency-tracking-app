edd8e860e0841130271d5c12ca9cdb719798f812
Arturs <arturs.vanags320@gmail.com>
2024-02-19 15:36:37 +0200

    Add toDtoMapper function to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
+import { CoinDTO } from "@/types";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function toDtoMapper(coin: CoinDTO) {
+  return {
+    slug: coin.slug,
+    name: coin.name,
+    symbol: coin.symbol,
+    ...coin.coinData,
+  };
 }
