65064a67c20f87fa39ae6ba633f60e0aaf6de169
Arturs <arturs.vanags320@gmail.com>
2024-02-25 20:18:30 +0200

    Add additional properties to mapAPICoinDataToDTO function

--- src/lib/mapper.ts
+++ src/lib/mapper.ts
@@ -14,4 +14,11 @@ export const mapAPICoinToDTO = (coin: APICoinList): CreateCoinDTO => ({
   export const mapAPICoinDataToDTO = (coinData: CoinDataAPI): CreateCoinDataDTO => ({
     marketCap: coinData.market_cap,
     volume: coinData.total_volume,
+    totalSupply: coinData.total_supply,
+    maxSupply: coinData.max_supply,
+    totalVolume: coinData.total_volume,
+    circulatingSupply: coinData.circulating_supply,
+    high24h: coinData.high_24h,
+    low24h: coinData.low_24h,
+    marketCapRank: coinData.market_cap_rank,
   });
