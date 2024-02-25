8e0d929185d31ec62ec610baf0e958e8017f0127
Arturs <arturs.vanags320@gmail.com>
2024-02-25 20:17:37 +0200

    Refactor API endpoint and add ping functionality

--- src/lib/repositories/apiRepository.tsx
+++ src/lib/repositories/apiRepository.tsx
@@ -2,8 +2,10 @@
 
 import { CoinDataAPI, APICoinList } from '@/types';
 import { getLargeDataFromRedis, setLargeDataInRedis } from '@/lib/redis';
+import { settingsService } from '@/lib/services/settingsService';
+
 /**
- * 
+ *
  * @returns A promise that resolves to an array of APICoinList objects.
  */
 export async function fetchAPICoins(): Promise<APICoinList[]> {
@@ -29,16 +31,22 @@ export async function fetchAPICoins(): Promise<APICoinList[]> {
 }
 /**
  * Fetches coin market data from the API based on the provided coin IDs.
- * 
+ *
  * @param ids The IDs of the coins to fetch data for.
  * @returns A promise that resolves to an array of CoinDataAPI objects.
  */
 export async function fetchAPICoinData(ids: string): Promise<CoinDataAPI[]> {
   try {
-    const response = await fetch(
-      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=${ids}&locale=en`,
-      { cache: 'no-cache' },
-    );
+    const proApiKey = await settingsService.getAPI();
+
+    const baseURL = proApiKey
+      ? `https://pro-api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=${ids}&locale=en`
+      : `https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=${ids}&locale=en`;
+
+    const queryString = proApiKey ? `&x_cg_pro_api_key=${proApiKey}` : '';
+    const finalURL = `${baseURL}${queryString}`;
+
+    const response = await fetch(finalURL, { cache: 'no-cache' });
     if (!response.ok) {
       throw new Error(`API request failed: ${response.statusText}`);
     }
@@ -47,5 +55,19 @@ export async function fetchAPICoinData(ids: string): Promise<CoinDataAPI[]> {
   } catch (error) {
     console.error('Failed to fetch coin market data from API.', error);
     throw new Error('Failed to fetch coin market data from API.');
+  }
+}
+/**
+ * Pings the API to check if it is reachable.
+ *
+ * @returns A promise that resolves to a boolean indicating if the API is reachable.
+ */
+export async function pingAPI(): Promise<boolean> {
+  try {
+    const response = await fetch('https://api.coingecko.com/api/v3/ping');
+    return response.ok;
+  } catch (error) {
+    console.error('Failed to ping the API.', error);
+    throw new Error('Failed to ping the API.');
   }
 }
