47dd72896114aef75e9dfd2649163785a7749ef7
Arturs <arturs.vanags320@gmail.com>
2024-02-25 20:19:04 +0200

    Refactor home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,14 @@ import CryptocurrencyTemplate from '@/components/main/template';
 
 export default function Home() {
   return (
-    <div className="max-h-screen min-h-screen container">
-      <div className="mx-auto my-auto">
+    <div className="flex min-h-screen flex-col items-center">
+      <div className="mt-32 w-full pb-4 text-center">
+        <h1 className="text-4xl font-bold">Cryptocurrency Tracking App</h1>
+        <p className="text-lg text-muted-foreground">
+          A simple cryptocurrency tracking app using CoinGecko API
+        </p>
+      </div>
+      <div className="mt-4 w-full max-w-4xl px-4">
         <CryptocurrencyTemplate />
       </div>
     </div>
